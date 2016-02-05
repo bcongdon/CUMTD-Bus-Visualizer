0ae639032d16852ffc7705bdaaba8c35d4ef4925
Benjamin Congdon <bencongdon96@gmail.com>
2016-02-05 11:23:36 -0600

    Bus data cached to reduce API calls

---
+++ app/bus_client.py
@@ -0,0 +1,51 @@
+import os
+import requests
+import json
+
+from datetime import timedelta
+from datetime import datetime
+
+class BusClient():
+
+	last_request = None
+	data = None
+
+	def get_busses(self):
+		if self.last_request is None or (self.last_request - datetime.today()) > timedelta(minutes=1):
+			self.data = self.get_busses_raw()
+			self.last_request = datetime.today()
+		else:
+			print "Using cached data; too soon to make new request"
+		return self.data
+
+	def get_busses_raw(self):
+	    key = os.environ.get('API_KEY',"")
+	    if key == "":
+	        print "Could not load API_KEY"
+	        return
+
+	    print "Making request to CUMTD"
+	    req = requests.get("https://developer.cumtd.com/api/v2.2/json/GetVehicles?key=" + key)
+	    data = req.json()
+
+	    ret = list()
+	    avg_lat, avg_lon, act_len = 0, 0, 0
+
+	    if not 'vehicles' in data:
+	        print "Data not in expected format"
+	        return
+
+	    for vehicle in data['vehicles']:
+	        if not 'trip' in vehicle:
+	            success = False
+	            return "JSON error"
+	        ret.append(vehicle)
+	        if abs(vehicle['location']['lon']) > 0.1: 
+	            avg_lon += vehicle['location']['lon']
+	            avg_lat += vehicle['location']['lat']
+	            act_len += 1
+	        else:
+	            print('caught')
+	    avg_lat /= act_len
+	    avg_lon /= act_len
+	    return (ret,avg_lon,avg_lat,True)
--- app/views.py
+++ app/views.py
@@ -1,48 +1,19 @@
 from app import app
-import os
 import json
 import datetime
 
-import requests
+from bus_client import BusClient
 from flask import render_template
+
+client = BusClient()
 
 @app.route('/')
 @app.route('/index')
 def index():
-    data = get_busses()
+    global client
+    data = client.get_busses()
     if data is not None:
         return render_template("map.html",points = data[0], lon = data[1], lat = data[2])
     else:
         return "Error"
 
-
-def get_busses():
-    key = os.environ.get('API_KEY',"")
-    if key == "":
-        print "Could not load API_KEY"
-        return
-
-    req = requests.get("https://developer.cumtd.com/api/v2.2/json/GetVehicles?key=" + key)
-    data = req.json()
-
-    ret = list()
-    avg_lat, avg_lon, act_len = 0, 0, 0
-
-    if not 'vehicles' in data:
-        print "Data not in expected format"
-        return
-
-    for vehicle in data['vehicles']:
-        if not 'trip' in vehicle:
-            success = False
-            return "JSON error"
-        ret.append(vehicle)
-        if abs(vehicle['location']['lon']) > 0.1: 
-            avg_lon += vehicle['location']['lon']
-            avg_lat += vehicle['location']['lat']
-            act_len += 1
-        else:
-            print('caught')
-    avg_lat /= act_len
-    avg_lon /= act_len
-    return (ret,avg_lon,avg_lat,True)
